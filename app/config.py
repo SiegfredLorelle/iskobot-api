cbcbf30c1d78721db2c199b6eb4e7d1f490d9f0a
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2025-01-03 19:32:21 +0800

    Create config.py to improve file structure

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+class Config:
+    REGION = os.getenv("REGION", "")
+    DB_INSTANCE_NAME = os.getenv("DB_INSTANCE_NAME", "")
+    DB_USER = os.getenv("DB_USER", "")
+    DB_NAME = os.getenv("DB_NAME", "")
+    DB_PASS = os.getenv("DB_PASS", "")
+    GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME", "")
