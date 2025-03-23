3794ffd4fcc352faa1d59f3fb0d1ed8b415250bb
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2025-03-23 12:57:53 +0800

    Connect to Supabase vector db instead GCP CloudSQL

--- app/config.py
+++ app/config.py
@@ -4,10 +4,16 @@ import os
 load_dotenv()
 
 class Config:
+    # Google Cloud Platform
     REGION = os.getenv("REGION", "")
+    GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME", "")
     DB_INSTANCE_NAME = os.getenv("DB_INSTANCE_NAME", "")
+
+    # Database
     DB_USER = os.getenv("DB_USER", "")
     DB_NAME = os.getenv("DB_NAME", "")
     DB_PASS = os.getenv("DB_PASS", "")
-    GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME", "")
+    DB_HOST = os.getenv("DB_HOST", "")
+    DB_PORT = os.getenv("DB_PORT", "")
+
     CONQUI_XTTS_ID = os.getenv("CONQUI_XTTS_ID", "")
