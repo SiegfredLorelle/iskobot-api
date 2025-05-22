70f8cd7f98010d17fb22be3a73ed64a8e50b9c9e
Siegfred Lorelle Mina <siegfredlorelle09@gmail.com>
2025-05-22 09:12:32 +0800

    Update model to allow more token consumptions

--- app/server.py
+++ app/server.py
@@ -82,7 +82,7 @@ Your answer: """
 
 # (4) Initialize LLM
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.0-flash",
     temperature=0.2,
     max_output_tokens=500,
     top_k=40,
